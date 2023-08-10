6b4fbfe5fbec33906ef0fe668a74b2e423449338
Abilash A R <abilash.ar@netcracker.com>
2023-08-10 10:32:05 +0530

    rhytm-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui/screens/rhythm.dart';
 // import 'package:flutter_ui/screens/design_palette.dart';
-import 'package:flutter_ui/screens/wanderer.dart';
+// import 'package:flutter_ui/screens/wanderer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Wanderer(),
+      home: const Rhythm(),
     );
   }
 }
